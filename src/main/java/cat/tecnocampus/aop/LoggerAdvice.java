040a5cd3cb53aaae6379d17b3b92060205f9d0ec
Marc Viladot <mviladot@edu.tecnocampus.cat>
2017-10-15 21:09:41 +0200

    Acabat

--- src/main/java/cat/tecnocampus/aop/LoggerAdvice.java
+++ src/main/java/cat/tecnocampus/aop/LoggerAdvice.java
@@ -1,6 +1,8 @@
 package cat.tecnocampus.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
@@ -30,4 +32,19 @@ public class LoggerAdvice {
 
     @Pointcut("execution(* cat.tecnocampus.useCases.UserUseCases.insertBatch(..))")
     public void pointcutInsertBatch() { }
+
+    @Around("pointcutInsertBatch()")
+    public int[] dealRequestParam(ProceedingJoinPoint jp) {
+
+        try {
+            logger.info("before multiple inser");
+            int[] res = (int[]) jp.proceed();
+            logger.info("after multiple insert");
+            return res;
+        } catch (Throwable throwable) {
+            logger.error("Showing notes: Something went wrong");
+            throwable.printStackTrace();
+            return new int[]{};
+        }
+    }
 }
